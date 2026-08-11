7a745377e4c7bfd7538ef41209cbcb2d97185e90
Dean Ruina <dean.ruina@csc.fi>
2026-08-11 08:25:18 +0300

    fix: responsive title box

--- swift_browser_ui_frontend/src/components/BrowserSecondaryNavbar.vue
+++ swift_browser_ui_frontend/src/components/BrowserSecondaryNavbar.vue
@@ -11,6 +11,7 @@
           v-bind="active"
           :items.prop="mappedProjects"
           :label="$t('message.selectProj')"
+          :style="{ width: selectWidth }"
           placeholder="Select project"
           return-value
           hide-details
@@ -20,12 +21,12 @@
       </div>
       <div
         v-if="!multipleProjects"
-        class="nav-item column"
+        class="nav-item single-project"
       >
         <p class="label">
           {{ $t("message.currentProj") }}
         </p>
-        <p class="project-number">
+        <p class="project-full-title">
           {{ getProjectStr(active) }}
         </p>
       </div>
@@ -137,6 +138,14 @@ export default {
         name: this.getProjectStr(project),
         value: project.id,
       }));
+    },
+    selectWidth() {
+      const min = 300;
+      const max = 600;
+      const title = this.getProjectStr(this.active);
+      let width = Math.ceil(title.length / 10) * 100;
+      width = width > max ? max : width < min ? min : width;
+      return width + "px";
     },
     isUploading() {
       return this.$store.state.isUploading;
@@ -285,19 +294,12 @@ c-toasts {
   width: fit-content;
 }
 
-.select-project, .column {
-  min-width: 15rem;
-  flex: 0.5;
-}
-
-c-select {
-  flex: 1;
-}
-
 @media screen and (max-width: 767px) {
-  .select-project, .column {
+  .select-project, .single-project {
     width: 100%;
-    flex: auto;
+  }
+  .select-project c-select {
+    max-width: 100%;
   }
 }
 
@@ -356,14 +358,8 @@ c-select {
   border-bottom-color: white;
 }
 
-.column {
-  flex-direction: column;
-  padding: 0 0 0 1.5rem;
-  color: var(--csc-dark);
-}
-
-.project-number {
-  font-size: 0.875rem;
+.project-full-title {
+  margin-top: 0.5rem;
 }
 
 .label {
